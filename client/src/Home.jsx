04d2152591e9d16d67befef632cd2b121ddb9023
Roseanna Tenijieth <roseannatenijieth25@gmail.com>
2025-04-15 14:30:57 -0700

    Adds message form.

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -5,28 +5,38 @@ const Home = () => {
   const socket = io(import.meta.env.VITE_API_SERVER_URL)
 
   const [messages, setMessages] = useState([])
+  const [message, setMessage] = useState('')
 
   socket.on('chat message', (msg) => {
     setMessages([...messages, msg])
   })
 
   const handleMessage = () => {
-    console.log('handleMessage')
-    socket.emit('chat message', 'hard-coded message')
+    //Emit message
+    socket.emit('chat message', message)
+    //Clear message
+    setMessage('')
   }
   return (
     <div>
       <h1>Home</h1>
       
             {messages.map((message, index) => (
-               <div className="flex">
+               <div key={index} className="flex">
               <div className="relative w-3/4">{message}</div>
               </div> 
             ))}
       
       <div className="flex">
         <div className="relative w-3/4">
-          <input type="search" id="search-dropdown" className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg rounded-s-gray-100 rounded-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required />
+          <input
+            value={message}
+            onChange={(e) => setMessage(e.target.value)} 
+            type="search" 
+            id="search-dropdown" 
+            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg rounded-s-gray-100 rounded-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" 
+            placeholder="Search" 
+            required />
           <button
             onClick={handleMessage}
             type="button"
